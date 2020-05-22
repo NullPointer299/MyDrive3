52d10ab8bd544036d2ec2da939de380831f9694d
H.N <nullpointer299@gmail.com>
2020-05-22 18:52:04 +0900

    first commit

---
+++ src/main/java/com/fjb/mydrive3/Mydrive3Application.java
@@ -0,0 +1,13 @@
+package com.fjb.mydrive3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mydrive3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mydrive3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fjb/mydrive3/Mydrive3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fjb.mydrive3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mydrive3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
